aabc71a8c3c704776e6a889991556b7613dcc4cf
ne-salberti <albertistefano94@gmail.com>
2017-06-29 11:16:01 +0200

    inizio dataloader

---
+++ src/service/DataLoader.java
@@ -0,0 +1,44 @@
+package service;
+
+import javafx.scene.shape.Path;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by alberti stefano on 29/06/2017.
+ */
+public class DataLoader {
+
+    private DBConn db;
+
+    public DataLoader(DBConn db) {this.db = db;}
+
+    public void caricaFile(String p) throws IOException {
+
+        List<String> list = new ArrayList<>();
+        FileReader f;
+        f = new FileReader(p);
+        BufferedReader b;
+        b = new BufferedReader(f);
+        String s;
+        try {
+            while (true) {
+                s = b.readLine();
+                list.add(s);
+                if (s == null)
+                    break;
+                String file = "";
+                for (int i = 0; i < list.size(); i++) {
+                    file = list.get(i);
+                }//db.doSQL(file);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
